6e26459cd24945a6ed71ebadb5a634c99265ce52
Thomas Abplanalp <info@tasoft.ch>
2019-10-31 18:59:49 +0100

    FIX: make sql usable for sqlite
    Signed-off-by: Thomas Abplanalp <info@tasoft.ch>

--- src/UserSystem/UserProvider.php
+++ src/UserSystem/UserProvider.php
@@ -102,7 +102,58 @@ WHERE username = ?", [$username]) as $group) {
             $roles = [];
             $uid = $user["id"];
 
-            foreach($this->PDO->select("SELECT DISTINCT 
+            if($this->PDO->getAttribute(PDO::ATTR_DRIVER_NAME) == 'sqlite') {
+                foreach($this->PDO->select("SELECT DISTINCT 
+CASE
+      WHEN R10.id IS NOT NULL
+      THEN R10.name || '.' || R9.name || '.' || R8.name || '.' || R7.name || '.' || R6.name || '.' || R5.name || '.' || R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R9.id IS NOT NULL
+      THEN R9.name || '.' || R8.name || '.' || R7.name || '.' || R6.name || '.' || R5.name || '.' || R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R8.id IS NOT NULL
+      THEN R8.name || '.' || R7.name || '.' || R6.name || '.' || R5.name || '.' || R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R7.id IS NOT NULL
+      THEN R7.name || '.' || R6.name || '.' || R5.name || '.' || R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R6.id IS NOT NULL
+      THEN R6.name || '.' || R5.name || '.' || R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R5.id IS NOT NULL
+      THEN R5.name || '.' || R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R4.id IS NOT NULL
+      THEN R4.name || '.' || R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R3.id IS NOT NULL
+      THEN R3.name || '.' || R2.name || '.' || R1.name
+
+      WHEN R2.id IS NOT NULL
+      THEN R2.name || '.' || R1.name
+
+      ELSE R1.name
+END as role
+FROM SKY_ROLE AS R1
+LEFT JOIN SKY_ROLE AS R2 ON R2.id = R1.parent
+LEFT JOIN SKY_ROLE AS R3 ON R3.id = R2.parent
+LEFT JOIN SKY_ROLE AS R4 ON R4.id = R3.parent
+LEFT JOIN SKY_ROLE AS R5 ON R5.id = R4.parent
+LEFT JOIN SKY_ROLE AS R6 ON R6.id = R5.parent
+LEFT JOIN SKY_ROLE AS R7 ON R6.id = R6.parent
+LEFT JOIN SKY_ROLE AS R8 ON R6.id = R7.parent
+LEFT JOIN SKY_ROLE AS R9 ON R6.id = R8.parent
+LEFT JOIN SKY_ROLE AS R10 ON R6.id = R9.parent
+
+LEFT JOIN SKY_GROUP_ROLE ON SKY_GROUP_ROLE.role = R1.id
+LEFT JOIN SKY_USER_GROUP ON SKY_GROUP_ROLE.`groupid` = SKY_USER_GROUP.`groupid`
+LEFT JOIN SKY_USER_ROLE ON SKY_USER_ROLE.role = R1.id
+WHERE SKY_USER_ROLE.user = $uid OR SKY_USER_GROUP.user = $uid
+ORDER BY role") as $record) {
+                    $roles[] = $record["role"];
+                }
+            } else {
+                foreach($this->PDO->select("SELECT DISTINCT 
 CASE
       WHEN R10.id IS NOT NULL
       THEN CONCAT(R10.name, '.', R9.name, '.', R8.name, '.', R7.name, '.', R6.name, '.', R5.name, '.', R4.name, '.', R3.name, '.', R2.name, '.', R1.name)
@@ -149,8 +200,12 @@ LEFT JOIN SKY_USER_GROUP ON SKY_GROUP_ROLE.`groupid` = SKY_USER_GROUP.`groupid`
 LEFT JOIN SKY_USER_ROLE ON SKY_USER_ROLE.role = R1.id
 WHERE SKY_USER_ROLE.user = $uid OR SKY_USER_GROUP.user = $uid
 ORDER BY role") as $record) {
-                $roles[] = $record["role"];
+                    $roles[] = $record["role"];
+                }
             }
+
+
+            var_dump($roles);
 
             return new User($user, $roles);
         }
